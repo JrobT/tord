427fcfef70f7a91ef7aac78edf712f123dd707aa
Jack <jack@trute.co.uk>
2020-02-26 17:46:52 +0000

    Test test database

--- website/website/settings.py
+++ website/website/settings.py
@@ -89,14 +89,12 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.{}'.format(
-            getenv('DATABASE_ENGINE')
-        ),
-        'NAME': getenv('DATABASE_NAME'),
-        'USER': getenv('DATABASE_USERNAME'),
-        'PASSWORD': getenv('DATABASE_PASSWORD'),
-        'HOST': getenv('DATABASE_HOST'),
-        'PORT': getenv('DATABASE_PORT'),
+        'ENGINE': getenv('DATABASE_ENGINE', ''),
+        'NAME': getenv('DATABASE_NAME', ''),
+        'USER': getenv('DATABASE_USERNAME', ''),
+        'PASSWORD': getenv('DATABASE_PASSWORD', ''),
+        'HOST': getenv('DATABASE_HOST', ''),
+        'PORT': getenv('DATABASE_PORT', ''),
         'OPTIONS': json.loads(
             getenv('DATABASE_OPTIONS', '{}')
         ),
