80eb8c9917dcc1d56b6823cc2fe4dfb05d01d8ae
Jack <jack@trute.co.uk>
2020-03-03 16:47:37 +0000

    Add tests to cover post-detail view

--- website/tests/test_blog/unit/test_views.py
+++ website/tests/test_blog/unit/test_views.py
@@ -13,10 +13,14 @@ def create_category():
 
     def _make_create_category(title):
         category = Category(title=title)
+        category.save()
         created_categories.append(category)
         return category
 
     yield _make_create_category
+
+    for category in created_categories:
+        category.delete()
 
 
 @pytest.fixture
@@ -26,10 +30,14 @@ def create_post():
 
     def _make_create_post(title, body, category):
         post = Post(title=title, body=body, category=category)
+        post.save()
         created_posts.append(post)
         return post
 
     yield _make_create_post
+
+    for post in created_posts:
+        post.delete()
 
 
 def test_blog_app():
@@ -50,11 +58,33 @@ def test_create_status(client):
 
 
 @pytest.mark.django_db
-def test_create_post(create_category, create_post):
-    category = create_category("Test")
-    assert isinstance(category, Category)
-    assert category.title == "Test"
-    post = create_post("Test 1", "# Here is a body title", category)
-    assert isinstance(post, Post)
-    assert post.title == "Test 1"
-    assert markdown.markdown(post.body) == "<h1>Here is a body title</h1>"
+@pytest.mark.parametrize("category_title", ["test", "test_category"])
+@pytest.mark.parametrize(
+    "post_title,post_body,post_body_md",
+    [
+        ("test", "# Here is a body title", "<h1>Here is a body title</h1>"),
+        (
+            "test_post",
+            "**Here is a body title**",
+            "<p><strong>Here is a body title</strong></p>",
+        ),
+    ],
+)
+def test_detail_status(
+    client,
+    create_category,
+    create_post,
+    category_title,
+    post_title,
+    post_body,
+    post_body_md,
+):
+    response = client.get(reverse("post-view", args=[post_title]))
+    assert response.status_code == 404
+    category = create_category(category_title)
+    assert str(category) == category_title
+    post = create_post(post_title, post_body, category)
+    assert str(post) == post_title
+    assert markdown.markdown(post.body) == post_body_md
+    response = client.get(reverse("post-view", args=[post.slug]))
+    assert response.status_code == 200
