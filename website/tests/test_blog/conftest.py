87b504cb34604599553895e705cf5ab6e9ed2e8b
Jack <jack@trute.co.uk>
2020-03-04 21:13:20 +0000

    Automated testing improvements for edit view

--- website/tests/test_blog/conftest.py
+++ website/tests/test_blog/conftest.py
@@ -3,6 +3,7 @@ import django
 import pytest
 from os import getenv
 from django.conf import settings
+from blog.models import Category, Post
 
 
 def pytest_configure():
@@ -21,3 +22,37 @@ def django_db_setup():
         "PORT": getenv("DATABASE_PORT", "5432"),
         "OPTIONS": json.loads(getenv("DATABASE_OPTIONS", "{}")),
     }
+
+
+@pytest.fixture
+def create_category():
+
+    created_categories = []
+
+    def _make_create_category(title):
+        category = Category(title=title)
+        category.save()
+        created_categories.append(category)
+        return category
+
+    yield _make_create_category
+
+    for category in created_categories:
+        category.delete()
+
+
+@pytest.fixture
+def create_post():
+
+    created_posts = []
+
+    def _make_create_post(title, body, category):
+        post = Post(title=title, body=body, category=category)
+        post.save()
+        created_posts.append(post)
+        return post
+
+    yield _make_create_post
+
+    for post in created_posts:
+        post.delete()
--- website/tests/test_blog/unit/test_views.py
+++ website/tests/test_blog/unit/test_views.py
@@ -1,43 +1,7 @@
 import pytest
-import markdown
 from django.apps import apps
 from django.urls import reverse
 from blog.apps import BlogConfig
-from blog.models import Category, Post
-
-
-@pytest.fixture
-def create_category():
-
-    created_categories = []
-
-    def _make_create_category(title):
-        category = Category(title=title)
-        category.save()
-        created_categories.append(category)
-        return category
-
-    yield _make_create_category
-
-    for category in created_categories:
-        category.delete()
-
-
-@pytest.fixture
-def create_post():
-
-    created_posts = []
-
-    def _make_create_post(title, body, category):
-        post = Post(title=title, body=body, category=category)
-        post.save()
-        created_posts.append(post)
-        return post
-
-    yield _make_create_post
-
-    for post in created_posts:
-        post.delete()
 
 
 def test_blog_app():
@@ -48,43 +12,34 @@ def test_blog_app():
 @pytest.mark.django_db
 def test_index_status(client):
     response = client.get(reverse("post-list"))
-    assert response.status_code == 200
+    assert response.status_code == 200, "Should be status code 200"
 
 
 @pytest.mark.django_db
 def test_create_status(client):
     response = client.get(reverse("post-create"))
-    assert response.status_code == 200
+    assert response.status_code == 200, "Should be status code 200"
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("category_title", ["test", "test_category"])
-@pytest.mark.parametrize(
-    "post_title,post_body,post_body_md",
-    [
-        ("test", "# Here is a body title", "<h1>Here is a body title</h1>"),
-        (
-            "test_post",
-            "**Here is a body title**",
-            "<p><strong>Here is a body title</strong></p>",
-        ),
-    ],
-)
+@pytest.mark.parametrize("category_title", ["test"])
+@pytest.mark.parametrize("post_title,post_body", [("test", "# Here is a body title")])
 def test_detail_status(
-    client,
-    create_category,
-    create_post,
-    category_title,
-    post_title,
-    post_body,
-    post_body_md,
+    client, create_category, create_post, category_title, post_title, post_body
 ):
-    response = client.get(reverse("post-view", args=[post_title]))
-    assert response.status_code == 404
     category = create_category(category_title)
-    assert str(category) == category_title
     post = create_post(post_title, post_body, category)
-    assert str(post) == post_title
-    assert markdown.markdown(post.body) == post_body_md
     response = client.get(reverse("post-view", args=[post.slug]))
-    assert response.status_code == 200
+    assert response.status_code == 200, "Should be status code 200"
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("category_title", [("test")])
+@pytest.mark.parametrize("post_title,post_body", [("test", "# Here is a body title")])
+def test_edit_status(
+    client, create_category, create_post, category_title, post_title, post_body
+):
+    category = create_category(category_title)
+    post = create_post(post_title, post_body, category)
+    response = client.get(reverse("post-edit", args=[post.pk]))
+    assert response.status_code == 200, "Should be status code 200"
