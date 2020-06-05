9c1c86f168e0517935ecd0bf60cf58f7bf7e749f
Jack <jack@trute.co.uk>
2020-06-05 13:37:29 +0100

    Fix tests.

--- website/blog/context_processors.py
+++
@@ -1,8 +0,0 @@
-from blog.forms import EmailForm
-
-
-def email_form(request):
-    """
-    Context processor that provides the mailing list subscribe mini-form
-    """
-    return {'email_form': EmailForm(request.POST or None)}
--- website/tests/test_blog/unit/test_views.py
+++ website/tests/test_blog/unit/test_views.py
@@ -16,7 +16,14 @@ def test_index_status(client):
 
 
 @pytest.mark.django_db
-def test_create_status(client):
+@pytest.mark.parametrize(
+    "user_username,user_email,user_password", [("test", "test@test.com", "test")]
+)
+def test_create_status(client, create_user, user_username, user_email, user_password):
+    response = client.get(reverse("post-create"))
+    assert response.status_code == 403, "Should be status code 403"
+    create_user(user_username, user_email, user_password)
+    assert client.login(username=user_username, password=user_password) is True
     response = client.get(reverse("post-create"))
     assert response.status_code == 200, "Should be status code 200"
 
@@ -29,17 +36,33 @@ def test_detail_status(
 ):
     category = create_category(category_title)
     post = create_post(post_title, post_body, category)
-    response = client.get(reverse("post-view", args=[post.slug]))
+    response = client.get(reverse("post-detail", args=[post.slug]))
     assert response.status_code == 200, "Should be status code 200"
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize("category_title", [("test")])
 @pytest.mark.parametrize("post_title,post_body", [("test", "# Here is a body title")])
+@pytest.mark.parametrize(
+    "user_username,user_email,user_password", [("test", "test@test.com", "test")]
+)
 def test_edit_status(
-    client, create_category, create_post, category_title, post_title, post_body
+    client,
+    create_category,
+    create_post,
+    category_title,
+    post_title,
+    post_body,
+    create_user,
+    user_username,
+    user_email,
+    user_password,
 ):
     category = create_category(category_title)
     post = create_post(post_title, post_body, category)
+    response = client.get(reverse("post-edit", args=[post.pk]))
+    assert response.status_code == 403, "Should be status code 403"
+    create_user(user_username, user_email, user_password)
+    assert client.login(username=user_username, password=user_password) is True
     response = client.get(reverse("post-edit", args=[post.pk]))
     assert response.status_code == 200, "Should be status code 200"
