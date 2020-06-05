44e5a1cb6c9540eac6cde3e9b1ccdff163ea1b61
Jack <jack@trute.co.uk>
2020-06-05 11:21:52 +0100

    Add new models

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post, Category
+from blog.models import Post, Comment, Category, Email
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -7,9 +7,27 @@ class PostAdmin(admin.ModelAdmin):
     readonly_fields = ("posted", "edited")
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("name", "comment", "post", "created", "active")
+    list_filter = ("created", "active")
+    search_fields = ("name", "email", "comment")
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ("email", "subbed",)
+    list_filter = ("subbed",)
+    search_fields = ("email", "subbed",)
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Email, EmailAdmin)
