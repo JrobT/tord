f4fa26406a731d55a951aaa6deb46e55589a6710
Jack <jack@trute.co.uk>
2020-07-23 15:52:46 +0100

    Docker build working

--- website/website/settings.py
+++ website/website/settings.py
@@ -77,20 +77,17 @@ TEMPLATES = [
 
 DATABASES = {
     "default": {
-        "ENGINE": getenv("DATABASE_ENGINE", "django.db.backends.postgresql_psycopg2"),
-        "NAME": getenv("DATABASE_NAME", "truteordare"),
-        "USER": getenv("DATABASE_USERNAME", "truteordare"),
-        "PASSWORD": getenv("DATABASE_PASSWORD", "truteordare"),
-        "HOST": getenv("DATABASE_HOST", "127.0.0.1"),
-        "PORT": getenv("DATABASE_PORT", "5432"),
-        "OPTIONS": json.loads(getenv("DATABASE_OPTIONS", "{}")),
+        "ENGINE": getenv("DATABASE_ENGINE"),
+        "NAME": getenv("POSTGRES_DB"),
+        "USER": getenv("POSTGRES_USER"),
+        "PASSWORD": getenv("POSTGRES_PASSWORD"),
+        "HOST": getenv("DATABASE_HOST"),
+        "PORT": getenv("DATABASE_PORT")
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
@@ -104,11 +101,27 @@ USE_L10N = True
 USE_TZ = True
 
 # Assets
-STATIC_URL = "http://127.0.0.1:8080/"
-STATICFILES_DIRS = ['dist']
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (
+    join(PROJECT_DIR, 'static'),
+)
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'CACHE': not DEBUG,
+        'BUNDLE_DIR_NAME': 'bundle/',
+        'STATS_FILE': join(PROJECT_DIR,
+            'static/bundle/webpack-stats.json'),
+        'POLL_INTERVAL': 0.1,
+        'TIMEOUT': None,
+        'IGNORE': [r'.+\.hot-update.js', r'.+\.map'],
+        'LOADER_CLASS': 'webpack_loader.loader.WebpackLoader',
+    }
+}
 
 # Web Apps
 PACKAGE_APPS = [
+    "webpack_loader",
     "markdownx",
     "profanity"
 ]
